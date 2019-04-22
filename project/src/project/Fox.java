c87c003b15d3302b59209d2c5e54c34f458cc526
Hannah Bridge <hbridge@udel.edu>
2019-04-21 22:03:19 -0400

    fox

--- project/src/project/Fox.java
+++ project/src/project/Fox.java
@@ -44,4 +44,46 @@ public class Fox{
 		
 	}
 
+	public int getXloc() {
+		return xloc;
+	}
+
+	public void setXloc(int xloc) {
+		this.xloc = xloc;
+	}
+
+	public int getYloc() {
+		return yloc;
+	}
+
+	public void setYloc(int yloc) {
+		this.yloc = yloc;
+	}
+
+	public int getXincr() {
+		return xincr;
+	}
+
+	public void setXincr(int xincr) {
+		this.xincr = xincr;
+	}
+
+	public int getYincr() {
+		return yincr;
+	}
+
+	public void setYincr(int yincr) {
+		this.yincr = yincr;
+	}
+
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(ArrayList<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+
 }
