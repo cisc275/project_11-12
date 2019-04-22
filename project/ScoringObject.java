2fed6641396688e765323d772fb764c4af29f010
Hannah Bridge <hbridge@udel.edu>
2019-04-21 21:06:10 -0400

    scoring object tests

--- project/ScoringObject.java
+++ project/ScoringObject.java
@@ -28,6 +28,70 @@ public class ScoringObject {
 		this.xloc += this.xIncr;
 		this.yloc += this.yIncr;
 	}
+
+	public int getXloc() {
+		return xloc;
+	}
+
+	public void setXloc(int xloc) {
+		this.xloc = xloc;
+	}
+
+	public int getYloc() {
+		return yloc;
+	}
+
+	public void setYloc(int yloc) {
+		this.yloc = yloc;
+	}
+
+	public int getxIncr() {
+		return xIncr;
+	}
+
+	public void setxIncr(int xIncr) {
+		this.xIncr = xIncr;
+	}
+
+	public int getyIncr() {
+		return yIncr;
+	}
+
+	public void setyIncr(int yIncr) {
+		this.yIncr = yIncr;
+	}
+
+	public int getImageWidth() {
+		return imageWidth;
+	}
+
+	public void setImageWidth(int imageWidth) {
+		this.imageWidth = imageWidth;
+	}
+
+	public int getImageHeight() {
+		return imageHeight;
+	}
+
+	public void setImageHeight(int imageHeight) {
+		this.imageHeight = imageHeight;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public int getPointValue() {
+		return pointValue;
+	}
+
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
 	
 	
 }
